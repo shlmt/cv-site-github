3836a2d860acfabf9e34af08a6193392777c776c
shlmt <sh7618sh@gmail.com>
2025-02-09 16:25:02 +0200

    get repos from github api

---
+++ GithubApi/Controllers/GithubController.cs
@@ -0,0 +1,44 @@
+﻿using GithubService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GithubApi.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GithubController : ControllerBase
+    {
+        private readonly GitHubService _githubService;
+        private readonly string _githubToken;
+        public GithubController(IConfiguration configuration)
+        {
+            _githubService = new GitHubService();
+            _githubToken = configuration["GithubToken"];
+        }
+
+        [Route("repository")]
+        [HttpGet]
+        public async Task<IActionResult> GetRepository(string? repoName = null, string? userName = null, string? language = null)
+        {
+            var repos = await _githubService.SearchRepositories(repoName, userName, language);
+            if (repos == null)
+            {
+                return NotFound();
+            }
+            return Ok(repos);
+        }
+
+        [Route("portfolio")]
+        [HttpGet]
+        public async Task<IActionResult> GetPortfolio()
+        {
+            var repos = await _githubService.GetPortfolio(_githubToken);
+            if (repos == null)
+            {
+                return NotFound();
+            }
+            return Ok(repos);
+        }
+    }
+}
---
+++ GithubService/GithubService.cs
@@ -0,0 +1,49 @@
+﻿using Octokit;
+using System;
+
+namespace GithubService
+{
+    public class GitHubService
+    {
+        private readonly GitHubClient _client;
+        public GitHubService()
+        {
+            _client = new GitHubClient(new ProductHeaderValue("cv-github-app"));
+        }
+        public async Task<List<object>> GetPortfolio(string token)
+        {
+            _client.Credentials = new Credentials(token);
+            var repos = await _client.Repository.GetAllForCurrent();
+            var portfolio = await Task.WhenAll(repos.Select(async repo => new
+            {
+                repo.Name,
+                repo.Description,
+                repo.HtmlUrl,
+                Website = repo.Homepage,
+                repo.StargazersCount,
+                Topics = (await _client.Repository.GetAllTopics(repo.Owner.Login, repo.Name)).Names,
+                Languages = await _client.Repository.GetAllLanguages(repo.Owner.Login, repo.Name),
+                PullRequests = (await _client.PullRequest.GetAllForRepository(repo.Owner.Login, repo.Name)).Count,
+                CommitsCount = (await _client.Repository.Commit.GetAll(repo.Owner.Login, repo.Name)).Count,
+                LastCommit = (await _client.Repository.Commit.GetAll(repo.Owner.Login, repo.Name)).FirstOrDefault()?.Commit.Author.Date
+            }));
+            return portfolio.ToList<object>();
+        }
+        public async Task<List<Repository>> SearchRepositories(string? name, string? username, string? lang)
+        {
+            var queryParts = new List<string>();
+            if (!string.IsNullOrWhiteSpace(name))
+                queryParts.Add(name);
+            if (!string.IsNullOrWhiteSpace(username))
+                queryParts.Add($"user:{username}");
+            if (!string.IsNullOrWhiteSpace(lang))
+                queryParts.Add($"language:{lang}");
+            string query = string.Join(" ", queryParts);
+            if (string.IsNullOrWhiteSpace(query))
+                throw new ArgumentException("At least one search parameter must be provided.");
+            var req = new SearchRepositoriesRequest(query);
+            var res = await _client.Search.SearchRepo(req);
+            return res.Items.Take(5).ToList();
+        }
+    }
+}
